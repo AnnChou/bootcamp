ed7c1aec6b26ec7ea7ba3494bf6eb6aee35ed0b0
Zoe <doticus@gmail.com>
2016-03-31 22:48:36 -0400

    Added class exercises and a resources file

--- Week 04/Day 5/W4 - Day 5 Outline.md
+++ Week 04/Day 5/W4 - Day 5 Outline.md
@@ -4,8 +4,8 @@ This is an **[activity-heavy]** day.
 
 ### Goals
 1. Basic overview of classes
-2. Give students time to review challenging concepts
-3. Introduce potential GitHub projects for contributions
+2. Introduce validations with example
+3. Give students time to review challenging concepts
 
 ### Schedule
 #### 10:00-10:30, Stand-up session
@@ -16,19 +16,17 @@ File: classes1.rb, classes2.rb, classes3.rb
 
 #### 11:30-12:30,
 ##### Talk: What can classes do? Using initialized variables to calculate price
-File: classes4.rb, classesfinal.rb
+File: classes4.rb, classes5.rb
 
 #### 12:30-13:30,
 Lunch
 
 #### 13:30-14:30,
-##### Activity: Student review time for challenging concepts
-Open for students to ask for help
+##### Talk: Adding validations to our class
+File: classesfinal.rb
 
 #### 14:30-15:00, Bio-break
 
-#### 15:00-16:00,
-##### Activity: Finding projects to contribute to
-
-#### 16:00-18:00,
-##### Activity: create version of a store as exercise. Choice of store is up to the students (or expand on cafe script, for instance adding muffin flavors?)
+#### 15:00-18:00,
+##### Questions, ask students what topics they want to revise
+Possible exercises: add validation for coffee liquids (milk, cream, etc...), write a class for another cafe item (muffins, tea)
--- Week 04/Day 5/classesfinal.rb
+++ Week 04/Day 5/classesfinal.rb
@@ -1,5 +1,8 @@
-#Okay, let's go back and set up the hash we had before
-#Then we can find the prices in get_price that way
+#Our Coffee class now works pretty well, as long as everyone inputs information correctly
+#If we get any bad input, though, we don't have any safeguards in place (go back to classes5.rb and try to pass a non-existent size when instantiating your class)
+
+#Let's add some validations to help prevent our script from crashing
+#Rails has tools that will allow us to do this relatively easily with our data, but it is good practice to think about possible problems in our code
 
 class Coffee
 
@@ -8,7 +11,6 @@ class Coffee
     @liquid = liquid
     @sugar = sugar
 
-    #We are also going to initialize this hash to make it available in the class
     @coffee_prices = {
                     'small' => 1.50,
                     'medium' => 2.00,
@@ -16,14 +18,32 @@ class Coffee
                     }
   end
 
+  def validate_input
+    #Let's compare the input we get for size to the sizes that we have in our coffee_prices hash
+    #This method looks at the keys in the coffee_prices hash
+    if @coffee_prices.key?(@size)
+      #self here is the object that we are currently working with in our code
+      #For example, when the first example below runs this code, self is equal to our 'small' object
+      #If you want to call a method within a class on the object, then you use self
+      #In this case, if our coffee size is valid, we immediately do the get_price method
+      self.get_price
+    else
+      #If our coffee size is invalid, then we print this response instead
+      puts "Invalid size, please try again"
+    end
+  end
+
   def get_price
-    puts "Your total for your #{@size} coffee is #{sprintf('%.2f', @coffee_prices[@size])}"
+    puts "Your total for your #{@size} coffee is $#{sprintf('%.2f', @coffee_prices[@size])}"
   end
 end
 
 small = Coffee.new('small')
-puts small.get_price
 medium = Coffee.new('medium')
-puts medium.get_price
 large = Coffee.new('large')
-puts large.get_price
+invalid = Coffee.new('invalid')
+
+small.validate_input
+medium.validate_input
+large.validate_input
+invalid.validate_input
